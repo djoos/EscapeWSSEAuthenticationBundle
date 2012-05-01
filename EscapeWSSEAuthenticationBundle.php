26a10755c3a492ffa69a20d336d7a6f1154838b6
phiamo <phiamo@gmail.com>
2012-05-01 03:01:33 +0200

    Updated for 2.1 compat

--- EscapeWSSEAuthenticationBundle.php
+++ EscapeWSSEAuthenticationBundle.php
@@ -1,9 +1,17 @@
 <?php
-
 namespace Escape\WSSEAuthenticationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Escape\WSSEAuthenticationBundle\Security\Factory\Factory;
 
 class EscapeWSSEAuthenticationBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new Factory());
+    }
 }
